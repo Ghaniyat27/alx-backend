70727eaae34cf43f069b924a05bd6ea116375058
Ghaniyat27 <ganiyat4real1@gmail.com>
2024-07-24 13:45:24 +0000

    0x00-pagination Task

---
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""Deletion-resilient hypermedia pagination
+"""
+import csv
+from typing import Dict, List
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        """Initializes a new Server instance.
+        """
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
---
+++ 0x00-pagination/README.md
@@ -0,0 +1,7 @@
+Tasks
+0. Simple helper function
+1. Simple pagination
+2. Hypermedia pagination
+3. Deletion-resilient hypermedia pagination
+mandatory
+The goal here is that if between two queries, certain rows are removed from the dataset, the user does not miss items from dataset when changing page.
